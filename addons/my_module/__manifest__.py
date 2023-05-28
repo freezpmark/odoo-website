1f9281d3cd635e5b46a0cc6627d3c756ee389c95
Freezpmark <P.Markus1337@gmail.com>
2023-05-29 08:42:47 +0900

    [ADD] manifest and init files

---
+++ addons/my_module/__manifest__.py
@@ -0,0 +1,34 @@
+{
+    "name": "Personal Website",
+    "version": "1.0",
+    "sequence": 1,
+    "summary": "Personal website showcasing my portfolio, projects, courses and profile",
+    "description": """
+Title
+=====
+
+Subtitle
+--------
+
+This is *emphasis*, rendered in italics.
+This is **strong emphasis**, rendered in bold.
+This is a bullet list:
+- Item one.
+- Item two.
+
+    """,
+    "website": "https://www.linkedin.com/in/freezpmark/",
+    "author": "Peter Markus",
+    "category": "Website",
+    "depends": [
+        "website",
+        "website_slides",
+        "note",
+    ],
+    "data": [],
+    # 'demo': [],
+    # 'images': [],
+    # 'css': [],
+    # 'qweb': [],
+    # 'update': [],
+}
