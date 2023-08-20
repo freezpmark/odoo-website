a666d573163f499653329ea7956415e090ddcdae
Freezpmark <P.Markus1337@gmail.com>
2023-08-20 19:41:02 +0900

    ready odoo for website building

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Odoo-website
+# odoo-website
--- addons/my_module/__manifest__.py
+++ addons/my_module/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    "name": "Personal Website",
+    "name": "Freezpmark Website",
     "version": "1.0",
     "sequence": 1,
     "summary": "Personal website showcasing my portfolio, projects, courses and profile",
@@ -26,9 +26,68 @@ This is a bullet list:
         "note",
     ],
     "data": [],
-    # 'demo': [],
-    # 'images': [],
-    # 'css': [],
-    # 'qweb': [],
-    # 'update': [],
 }
+
+# application: Specifies whether the module is an application module.  # (default: False)
+# installable: Specifies whether the module can be installed. (default: True)
+# auto_install: Specifies whether the module should be automatically installed when its dependencies are met. (default: False)
+
+#     'description': """    (summary = Adds generic tax fields, and functionality for Slovak and Czech Republic)
+# VAT BASE
+# ==========
+# Adds tax fields, create tax functionality for Slovak and Czech Republic
+
+
+# For more information contact info@26house.com or visit https://www.26house.com
+
+# Pre viac informácií kontaktujte info@26house.com alebo navštívte https://www.26house.com
+#     """,
+# }
+
+# demo
+# reports
+# tests
+# theme
+# website
+# wizard
+
+# ######### ALL OPTS #########
+# name: The name of the module.
+# version: The version number of the module.
+# summary: A brief summary or description of the module.
+# description: A detailed description of the module.
+# author: The author or authors of the module.
+# website: The website URL related to the module.
+# category: The category or functional area to which the module belongs.
+# depends: A list of other modules that the module depends on.
+# data: The data directory is used to store XML files that define initial data to be loaded into the database when the module is installed. This can include configuration settings, default records, or any other data required for module functionality.
+# demo: The demo directory is used to define demo data for your module. It includes XML files that specify sample data to be loaded during the demonstration or testing of your module.
+# sequence: The sequence number or priority of the module when loading multiple modules. Set a lower value to prioritize the module.
+# images: A list of image files to be displayed for the module.
+# css: A list of CSS file paths to be included for the module.
+# qweb: A list of QWeb template file paths to be included for the module.
+# update: A list of update scripts to be executed when the module is updated.
+
+# security: The security directory contains XML files defining access control rules and permissions for your module. It specifies who can access specific models, fields, or actions within your module.
+# reports: The reports directory is used to store report templates and configurations. It includes files for defining the structure and layout of printable reports (in PDF or HTML) that can be generated from your module.
+# wizard: The wizard directory is used to define wizard models and their related views. Wizards are used for capturing user input and performing specific actions or processes.
+# tests: The tests directory is where you can write unit tests to ensure the functionality of your module. It typically includes Python files with test cases that verify the behavior of your module's models, views, and other components.
+# i18n: The i18n directory is used for internationalization and localization. It contains translation files for different languages, allowing your module to be translated into multiple languages.
+# models: This directory is used to define the Python models for your module. It includes classes that represent your data structure, define business logic, and handle interactions with the database.
+# views: The views directory contains XML files that define views and user interfaces for your module. It includes forms, lists, kanban views, and other UI elements.
+# static: This directory is used to store static files like JavaScript, CSS, and image files that are used in your module's web interface. These files are typically served directly to the client's browser.
+# templates: The templates directory holds XML or HTML templates used for generating dynamic content in your module's views. These templates can include placeholders and logic for rendering dynamic data.
+
+# doc: The doc directory can be used to store documentation related to your module. It can include user manuals, technical documentation, or any other relevant documentation files.
+# docs: Contains module documentation files.
+# mass_mailing: The mass_mailing directory is used to define specific email templates and configurations for mass mailing features. It includes files related to email campaigns, mailing lists, and email templates.
+# website: The website directory is used for additional customization and extensions related to the website module. It includes templates, static files, and other resources specifically designed for the website front-end.
+# theme: The theme directory is used to define custom themes for the website module. It includes CSS, JavaScript, and other assets required to create unique website themes and styles.
+# controllers: Contains Python files defining controllers for handling HTTP requests and providing business logic.
+# migrations: Contains Python files for database migrations.
+# info: Contains module information files.
+# hooks: Contains Python files defining module hooks and customizations.
+# scripts: Contains scripts or utilities related to the module.
+# data_dir: A directory used to store files uploaded through the module.
+# 'licence': 'LGPL-3',  # default
+# # company?
